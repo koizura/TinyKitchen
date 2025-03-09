211573e723845203dcec2ff30db21c35a680c84e
Han Li <han.li@berkeley.edu>
2025-03-09 12:55:25 -0700

    Cookbook

--- TinyKitchen/Assets/Scripts/Cookbook.cs
+++ TinyKitchen/Assets/Scripts/Cookbook.cs
@@ -4,6 +4,10 @@ public class Cookbook : MonoBehaviour
 {
     [SerializeField]
     private GameObject openBook;
+
+    private float mouseDownTime = 0;
+    [SerializeField]
+    private float mouseDownDuration = 0.5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,11 +17,15 @@ public class Cookbook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        bool isHit = Physics.Raycast(mouseRay.origin, mouseRay.direction, out RaycastHit draggableHit, 20.0f);
         if (Input.GetMouseButtonDown(0))
         {
-            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            bool isHit = Physics.Raycast(mouseRay.origin, mouseRay.direction, out RaycastHit draggableHit, 20.0f);
-            if (isHit && draggableHit.transform.gameObject == gameObject)
+            mouseDownTime = Time.time + mouseDownDuration;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (Time.time <= mouseDownTime && isHit && draggableHit.transform.gameObject == gameObject)
             {
                 SpawnBook();
             }
